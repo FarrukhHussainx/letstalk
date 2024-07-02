7b90b516af98a5654f023a1bc4872633fcb41c9d
farrukhhussainx <fa4ukhhuxxain101@gmail.com>
2024-07-02 22:40:28 +0500

    message_created

--- backend/model/auth.js
+++ backend/model/auth.js
@@ -1,31 +1,34 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  fullname: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female"],
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    dp: {
+      type: String,
+      default: "",
+    },
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  gender: {
-    type: String,
-    enum: ["Male", "Female"],
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  dp: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import authRouter from "./router/auth.js";
+import messageRouter from "./router/message.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 dotenv.config();
 connectDB();
 
@@ -14,5 +17,6 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 app.use("/api/auth", authRouter);
+app.use("/api/message", messageRouter);
 
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
